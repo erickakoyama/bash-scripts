3e5b35099fd962678be25decfd7992ee84529976
Ericka Koyama <ericka.koyama@liferay.com>
2020-01-15 15:02:04 -0800

    Update scripts

--- delete-unused-keys.sh
+++ delete-unused-keys.sh
@@ -1,39 +1,37 @@
 #!/bin/bash
 
-# Script for reporting unused language keys in the project
+# Script for finding and deleting unused language keys in the project
 
-# Get a list of all keys in Language.properties
-key_file_path=`find ./src/main -name "Language.properties"`;
-
-keys=`awk -F= '{print $1}' $key_file_path | sort -u`;
-
-# Would need to check if the key is used in any java clause operator files before deletion
-# This is in src/mainjava/com/liferay/osb/far/web/internal/clause/operator
-java_keys_path='./src/main/java/com/liferay/osb/faro/web/internal/clause/operator';
-
-# Keys used for java clause operators
-keys_in_java_source=`find $java_keys_path -name "*Operator.java" -exec grep -oh '_NAME = "[a-z-]*";' {} \; | \
-grep -oh \".*\" | tr -d \"`;
-# echo "$keys_in_java_source";
+# Get a sorted list of all keys in Language.properties
+key_file_path=`find ./src/main/resources/content -name "Language.properties"`;
+keys=`awk -F= '{print $1}' $key_file_path`;
 
 # Keys found in project js
-keys_in_js_source=`find ./src/main/js -name "*.js" -exec egrep -A1 'Liferay.Language.get\(' {} \; | \
-grep -oh \'.*\' | tr -d \'`;
-# echo "$keys_in_js_source";
-
-keys_in_all_sources=`echo "$keys_in_java_source"; echo "$keys_in_js_source" | sed '/^\s*$/d' | sort -u`;
+# -- multiline for capturing language keys that might be broken to the next line, -o to only print part of line that matches pattern
+keys_in_source=`find ./src/main/js \( -name "*.jsx" -o -name "*.js" -o -name "*.ts" -o -name "*.tsx" \) -exec pcregrep -o --multiline 'Liferay.Language.get\((\n|.)*?\)' {} \; | \
+# get only the part between quotations in Liferay.Language.get('...')
+grep -oh \'.*\' | \
+# delete any lines that contain capitals since they are probably not language keys
+sed "/[A-Z]/d" | \
+# delete the quotations surrounding the key '...'
+tr -d \' | \
+# sorted
+sort -u
+`;
 
 # Diff the two outputs and print line numbers only non-matches present in File2,
 # which represents language-keys that are not actually used anywwhere in the project.
-unused_line_nums=$(diff --new-line-format='>%dn:%L' <(echo "$keys_in_all_sources") <(echo "$keys") | \
+#
+# If we reverse the diff order, we should be able to find language keys in the source code that have no language key.
+unused_line_nums=$(diff --new-line-format='>%dn:%L' <(echo "$keys_in_source") <(echo "$keys") | \
 	 grep '^>.*$' | \
 	 awk -F: '{print $1}' | \
 	 tr -d '>');
 
-# Format line numbers for use in sed to delete each line
-sed_delete_str=`echo "$unused_line_nums" | awk '{print $0 "d;" }' | tr -d '\n' `;
+for key in $unused_line_nums; do
+	# go through each line num and replace that line with a blank line
+	sed -i '' -e "$key s/.*//" $key_file_path;
+done
 
-# Remove unused keys in $key_file_path
-sed -i '' $sed_delete_str $key_file_path;
-
-# also need to trim newline at end of edited file
+# delete all the blank lines
+sed -i '' -e '/^$/d' $key_file_path;
